46bff2a2b5628b4e4bc3f2febbc0e7f560cf657c
dannysitu <55365388+dannysitu@users.noreply.github.com>
2019-12-02 15:00:32 -0800

    Update intcodeProgram.py

--- intcodeProgram.py
+++ intcodeProgram.py
@@ -1,28 +1,4 @@
-def intcodeProgram(code):
-    x = 0
-  
-    length = len(code)
-
-    while x < length:
-      opcode = int(code[x])
-      saveLocation = int(code[x+3])
-      numberLocation1 = int(code[x+1])
-      numberLocation2 = int(code[x+2])
-      number1 = int(code[numberLocation1])
-      number2 = int(code[numberLocation2])
-
-      if opcode == 1:
-        code[saveLocation] = number1 + number2
-      elif opcode == 2:
-        code[saveLocation] = number1 * number2
-      elif opcode == 99:
-        return code[0]
-      else:
-        print("ERROR: INVALID CODE " + str(opcode))
-        break
-      
-      x+=4
-
+import intcode
 
 code = input("Input code(comma deliminated): ");
 
@@ -31,4 +7,4 @@ code = code.split(",");
 code[1] = 12
 code[2] = 2
 
-print(intcodeProgram(code));
+print(intcode.intcodeProgram(code));
