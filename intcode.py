8fa9902d398c8f8c9afb8836ed8c08df5391fe6b
dannysitu <55365388+dannysitu@users.noreply.github.com>
2019-12-02 15:01:07 -0800

    Create intcode.py

---
+++ intcode.py
@@ -0,0 +1,24 @@
+def intcodeProgram(code):
+    x = 0
+  
+    length = len(code)
+
+    while x < length:
+      opcode = int(code[x])
+      saveLocation = int(code[x+3])
+      numberLocation1 = int(code[x+1])
+      numberLocation2 = int(code[x+2])
+      number1 = int(code[numberLocation1])
+      number2 = int(code[numberLocation2])
+
+      if opcode == 1:
+        code[saveLocation] = number1 + number2
+      elif opcode == 2:
+        code[saveLocation] = number1 * number2
+      elif opcode == 99:
+        return code[0]
+      else:
+        print("ERROR: INVALID CODE " + str(opcode))
+        break
+      
+      x+=4
