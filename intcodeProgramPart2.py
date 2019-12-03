9d49a06fca7c2494687a4b7ffb0356081d4233f2
dannysitu <55365388+dannysitu@users.noreply.github.com>
2019-12-03 09:33:45 -0800

    Update intcodeProgramPart2.py

--- intcodeProgramPart2.py
+++ intcodeProgramPart2.py
@@ -1,20 +1,18 @@
 import intcode
 
-code = input("Input code(comma deliminated): ")
+intcodes = input("Input code(comma deliminated): ")
 
-code = code.split(",")
-
-orginalCode = code
+code = intcodes.split(",")
 
 x = 0
 noun = 0
 
-while x < 99:
+while x <= 99:
   code[1] = noun
   verb = 0
   y = 0
 
-  while y < 99:
+  while y <= 99:
     code[2] = verb
     output = intcode.intcodeProgram(code)
 
@@ -22,7 +20,8 @@ while x < 99:
       print(100 * noun + verb)
       break
     else:
-      code = orginalCode
+      code = intcodes.split(",")
+      code[1] = noun
 
     y = y + 1
     verb = y
